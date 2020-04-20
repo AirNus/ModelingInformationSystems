84840b410ae113f65412da3cc3979da23a8501ad
AirNus <58343285+AirNus@users.noreply.github.com>
2020-04-20 09:08:16 +0300

    Little edit

--- ModelingInformationSystems/Body.cs
+++ ModelingInformationSystems/Body.cs
@@ -1,5 +1,4 @@
-﻿using ByMarin;
-using System;
+﻿using System;
 using System.Globalization;
 using System.Linq;
 using System.Threading;
--- ModelingInformationSystems/Normal.cs
+++ ModelingInformationSystems/Normal.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ByMarin
+namespace ModelingInformationSystems
 {
     class Normal
     {
